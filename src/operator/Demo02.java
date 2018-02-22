b4a79a2a9f4d63d13c48a80d8e6a701cc6d16d1e
TWM <480086311@qq.com>
2018-02-22 21:59:07 +0800

    关系操作符

---
+++ src/operator/Demo02.java
@@ -0,0 +1,20 @@
+package operator;
+/**
+ * ¹ØÏµ²Ù×÷·û
+ * @author Administrator
+ *
+ */
+public class Demo02 {
+
+	public static void main(String[] args) {
+		int a = 5;
+		int b = 6;
+		int c = 5;
+		System.out.println(a > b);//false
+		System.out.println(a >= c);//true
+		
+		System.out.println(a ==b);//false
+		System.out.println(a!=b);//true
+	}
+
+}
