50b9598c8659a95edc6188639c39872f971bb957
TWM <480086311@qq.com>
2018-02-22 14:29:42 +0800

    表达式

---
+++ src/variable/quantity/Demo08.java
@@ -0,0 +1,23 @@
+package variable.quantity;
+
+public class Demo08 {
+
+	public static void main(String[] args) {
+		/**
+		 * 1、以;结尾的一段代码，即为一个表达式
+		 */
+		//每一句话都是一个表达式
+		int i = 5;
+		System.out.println(i);
+		/**
+		 * 2、;
+		 * 
+		 */
+		//一个空;也是一个表达式
+		;
+		;
+		;
+		;
+	}
+
+}
