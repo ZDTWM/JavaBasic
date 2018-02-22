b88f00d45cbed60a3f9871d109d229a7a1bdd15e
TWM <480086311@qq.com>
2018-02-22 13:56:56 +0800

    变量的命名规则

---
+++ src/variable/quantity/Demo05.java
@@ -0,0 +1,42 @@
+package variable.quantity;
+/**
+ * 变量的命名规则
+ * @author Administrator
+ *
+ */
+public class Demo05 {
+
+	public static void main(String[] args) {
+		/**
+		 * 1、命名规则
+		 */
+		int a = 5;
+		int a_12 = 5;
+		int $a43 = 5;
+		int a434 = 5;
+		//第一个是数字，是不行的
+		//int 43q = 5;
+		
+		/**
+		 * 2、使用完整的单词命名、而非缩写
+		 */
+		String name;
+		float hp ;
+		float armor;
+		int moveSpeed;
+		//不易于理解，不建议使用
+		String n ;
+		float h;
+		float ar;
+		int m;
+		
+		/**
+		 * 3、不能只使用关键字，但是可以包含关键字
+		 */
+		int class5 =5;
+		//int class = 5;
+				
+		
+	}
+
+}
