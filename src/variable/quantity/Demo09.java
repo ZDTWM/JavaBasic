24fd09e5fa29a167b20dda679d2e3757d0afda63
TWM <480086311@qq.com>
2018-02-22 14:34:30 +0800

    块： 从{ 开始 到对应的} 结束，即一个块

---
+++ src/variable/quantity/Demo09.java
@@ -0,0 +1,14 @@
+package variable.quantity;
+/**
+ * 块
+ * 从{ 开始 到对应的} 结束，即一个块
+ * @author Administrator
+ *
+ */
+public class Demo09 {//类对应的块
+
+	public static void main(String[] args) {//主方法对应的块
+		System.out.println("abc");
+	}
+
+}
