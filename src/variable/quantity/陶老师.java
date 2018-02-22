5b77b14b7b7e1b0a2879c59c09ffbca11cea62a1
TWM <480086311@qq.com>
2018-02-22 14:00:30 +0800

    变量的命名规则： 	中文也是可以用来命名变量的 	但是在实际工作中。。。别这么干。。。

---
+++ src/variable/quantity/陶老师.java
@@ -0,0 +1,18 @@
+package variable.quantity;
+/**
+ * 中文也是可以用来命名变量的 
+ * 但是在实际工作中。。。别这么干。。。
+ * @author Administrator
+ *
+ */
+public class 陶老师 {
+	
+	public void 开展教学工作(){
+		System.out.println("雅蠛蝶");
+	}
+	public static void main(String[] args) {
+		陶老师 陶文猛 = new 陶老师();
+		陶文猛.开展教学工作();
+	}
+
+}
