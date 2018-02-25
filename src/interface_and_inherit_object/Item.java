0641f70dd910a57e286bd0e982df25445bb6bfe2
TWM <480086311@qq.com>
2018-02-25 11:32:00 +0800

    finalize(): 	当一个对象没有任何引用指向的时候，它就满足垃圾回收的条件 	当它被垃圾回收的时候，它的finalize() 方法就会被调用。 	finalize() 不是开发人员主动调用的方法，而是由虚拟机JVM调用的。

---
+++ src/interface_and_inherit_object/Item.java
@@ -0,0 +1,32 @@
+package interface_and_inherit_object;
+/**
+ * finalize()
+ * 
+ * @author Administrator
+ *
+ */
+//当一个对象没有任何引用指向的时候，它就满足垃圾回收的条件
+//当它被垃圾回收的时候，它的finalize() 方法就会被调用。
+//finalize() 不是开发人员主动调用的方法，而是由虚拟机JVM调用的
+public class Item {
+	
+    public String name;
+    protected float hp;
+    
+    public void finalize(){
+    	System.out.println("这个英雄正在被回收");
+    }
+    
+    public static void main(String[] args) {
+		Item item;
+		for (int i = 0; i < 100000; i++) {
+            //不断生成新的对象
+            //每创建一个对象，前一个对象，就没有引用指向了
+            //那些对象，就满足垃圾回收的条件
+            //当，垃圾堆积的比较多的时候，就会触发垃圾回收
+            //一旦这个对象被回收，它的finalize()方法就会被调用
+			item = new Item();
+		}
+	}
+    
+}	
